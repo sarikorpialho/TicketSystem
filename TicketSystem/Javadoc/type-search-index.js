3f2078d9b986ad4ad3b98b3700b5ca86c3c00985
sarikorpialho <58845869+sarikorpialho@users.noreply.github.com>
2021-04-15 16:35:46 +0300

    Poistettu virheellinen javadoc.

--- TicketSystem/Javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"ticketSystem","l":"Booking"},{"p":"ticketSystem","l":"Customer"},{"p":"ticketSystem","l":"Flight"},{"p":"ticketSystem","l":"Seat"},{"p":"ticketSystem","l":"SeatClass"},{"p":"ticketSystem","l":"SeatTypes"},{"p":"ticketSystem","l":"UserInterface"}];updateSearchResults();
